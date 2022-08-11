e1fee2c32249293ce3e124d77b49d3ce284dc9b1
Alexandra <scribbo@mail.ru>
2022-08-11 19:36:49 +0300

    hw_2

---
+++ todo/frontend/src/components/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Menu = () => {
+    return(
+        <p>
+            <table>
+                <th>
+                    Главная
+                </th>
+                <th>
+                    Пользователи
+                </th>
+                <th>
+                    Задачи
+                </th>
+            </table>
+        </p>
+    )
+}
+
+export default Menu
